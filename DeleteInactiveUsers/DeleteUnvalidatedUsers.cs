46fabea8a85fe149dbe6b5d1f8faa076d2994b32
teecode <ogunseye.timilehin@gmail.com>
2017-12-13 12:26:18 +0100

    DIA first commit

---
+++ DeleteInactiveUsers/DeleteUnvalidatedUsers.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DeleteInactiveUsers
+{
+    public partial class DeleteUnvalidatedUsers : ServiceBase
+    {
+        public DeleteUnvalidatedUsers()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+
+        protected override void OnStart(string[] args)
+        {
+            
+            this.WriteToFile("Simple Service started {0}");
+            this.ScheduleService();
+        }
+
+        protected override void OnStop()
+        {
+            this.WriteToFile("Simple Service stopped {0}");
+            this.Schedular.Dispose();
+        }
+
+        private Timer Schedular;
+
+        public void ScheduleService()
+        {
+            try
+            {
+                Schedular = new Timer(new TimerCallback(SchedularCallback));
+                string mode = ConfigurationManager.AppSettings["Mode"].ToUpper();
+                this.WriteToFile("Simple Service Mode: " + mode + " {0}");
+
+                //Set the Default Time.
+                DateTime scheduledTime = DateTime.MinValue;
+
+                if (mode == "DAILY")
+                {
+                    //Get the Scheduled Time from AppSettings.
+                    scheduledTime = DateTime.Parse(System.Configuration.ConfigurationManager.AppSettings["ScheduledTime"]);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next day.
+                        scheduledTime = scheduledTime.AddDays(1);
+                    }
+                }
+
+                if (mode.ToUpper() == "INTERVAL")
+                {
+                    //Get the Interval in Minutes from AppSettings.
+                    int intervalMinutes = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalMinutes"]);
+
+                    //Set the Scheduled Time by adding the Interval to Current Time.
+                    scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next Interval.
+                        scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
+                    }
+                }
+
+                TimeSpan timeSpan = scheduledTime.Subtract(DateTime.Now);
+                string schedule = string.Format("{0} day(s) {1} hour(s) {2} minute(s) {3} seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+                this.WriteToFile("Simple Service scheduled to run after: " + schedule + " {0}");
+
+                DeleteUsers();
+
+                //Get the difference in Minutes between the Scheduled and Current Time.
+                int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
+
+                //Change the Timer's Due Time.
+                Schedular.Change(dueTime, Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Simple Service Error on: {0} " + ex.Message + ex.StackTrace);
+
+                //Stop the Windows Service.
+                using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController("SimpleService"))
+                {
+                    serviceController.Stop();
+                }
+            }
+        }
+
+        private void DeleteUsers()
+        {
+            try
+            {
+                UsersFetcher.DeleteCustomerCascade();
+                UsersFetcher.DeleteInACtiveCustomerCascade();
+            }
+            catch (Exception ex)
+            {
+
+                this.WriteToFile(ex.Message);
+            }
+        }
+
+        private void SchedularCallback(object e)
+        {
+            this.WriteToFile("Simple Service Log: {0}");
+            this.ScheduleService();
+        }
+
+        private void WriteToFile(string text)
+        {
+
+            try
+            {
+               //string xxx = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "ILOTTO" + Path.DirectorySeparatorChar + "GAMEOFDAYSERVICE" + Path.DirectorySeparatorChar + "GamesCreated.txt");
+               //xxx = Path.GetFullPath(xxx).Replace(@"\", @"\\");
+
+                
+
+
+               string path = System.Configuration.ConfigurationManager.AppSettings["LogFile"].ToString();
+                //Directory.GetDirectories(path);
+                FileInfo file = new FileInfo(path);
+                file.Directory.Create();
+                if (file.Exists && file.Length > 40000)
+                    file.Delete();
+                
+                    using (StreamWriter writer = new StreamWriter(path, true))
+                    {
+                        writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                        writer.Close();
+                    }
+                
+            }
+            catch (Exception )
+            {
+                
+            }
+        }
+    }
+}
---
+++ DeleteInactiveUsers/UsersFetcher.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Transactions;
+using System.Web;
+
+namespace DeleteInactiveUsers
+{
+    public static class UsersFetcher
+    {
+        static int cmdfetchmode;
+        public static List<Customer> getInactivecustomers()
+        {
+            try
+            {
+                string daysstring = System.Configuration.ConfigurationManager.AppSettings["InactivityDays"].ToString();
+
+                int days = int.Parse(daysstring);
+                
+                DateTime minussevendays = DateTime.Now.AddDays(-days);
+               GCL_LOTTODBCONTEXTDataContext db = new GCL_LOTTODBCONTEXTDataContext();
+               List<Customer> customers = db.Customers.Where(a => a.Customer_Category1.code == "OC" && a.Customer_Login.is_active == false && a.Customer_Login.is_verified == false && a.dateRegistered < minussevendays).ToList<Customer>();
+               return customers;
+            }
+            catch (Exception ex)
+            {
+                WriteToFile(ex.Message);
+                return null;
+            }
+        }
+
+
+        public static List<Customer> getVerifiedInactivecustomers()
+        {
+            try
+            {
+                string daysstring = System.Configuration.ConfigurationManager.AppSettings["VerifiedButInactivitydays"].ToString();
+
+                int days = int.Parse(daysstring);
+
+                DateTime minusdays = DateTime.Now.AddDays(-days);
+                GCL_LOTTODBCONTEXTDataContext db = new GCL_LOTTODBCONTEXTDataContext();
+                List<Customer> customers = db.Customers.Where(a => a.Customer_Category1.code == "OC" && a.Customer_Login.is_active == true && a.dateRegistered < minusdays && a.Customer_Transactions.Count <= 2 && a.Customer_Login.Customer_Login_Logs.Last().timein < minusdays  && a.Customer_Login.Customer_Login_Logs.Count <3).ToList<Customer>();
+                return customers;
+            }
+            catch (Exception ex)
+            {
+                WriteToFile(ex.Message);
+                return null;
+            }
+        }
+
+        public static void DeleteInACtiveCustomerCascade()
+        {
+            GCL_LOTTODBCONTEXTDataContext db = new GCL_LOTTODBCONTEXTDataContext();
+            //List<Daily_Game> DailyGAmes = new List<Daily_Game>();
+            string daysstring = System.Configuration.ConfigurationManager.AppSettings["VerifiedButInactivitydays"].ToString();
+
+            int days = int.Parse(daysstring);
+
+            DateTime minusdays = DateTime.Now.AddDays(-days);
+           // List<Customer> customers = db.Customers.Where(a => a.Customer_Category1.code == "OC" && a.Customer_Wallet.wallet_balance <= 200 && a.Customer_Login.Customer_Login_Logs.Count < 3 && a.Customer_Login.is_active == false && a.Customer_Login.is_verified == false && a.dateRegistered < minussevendays).ToList<Customer>();
+            List<Customer> customers = db.Customers.Where(a => a.Customer_Category1.code == "OC" && a.Customer_Wallet.wallet_balance <= 200 && a.Customer_Login.is_active == true && a.dateRegistered < minusdays && a.Customer_Transactions.Count <= 2 && a.Customer_Login.Customer_Login_Logs.OrderByDescending(b => b.id).FirstOrDefault().timein < minusdays && a.Customer_Login.Customer_Login_Logs.Count < 3).ToList<Customer>();
+            WriteToFile("Customer Count within " + days + " days => " + customers.Count());
+            for (int i = 0; i < customers.Count; i++)
+            {
+                Customer cust = customers[i];
+                try
+                {
+                    WriteToFile(cust.id + ": " + cust.Customer_Login.username + " begin delete");
+                    if (cust.Customer_Transactions.Count() < 3  && cust.Tickets.Count() == 0 && cust.Customer_Deposits.Count() < 5)
+                    {
+                        Customer_Login_Log lastlogin = cust.Customer_Login.Customer_Login_Logs.OrderByDescending(a => a.id).FirstOrDefault();
+                        if (lastlogin == null || (lastlogin.status == true && lastlogin.timein <= minusdays))
+                        {
+                            db.deleteCustomer(cust.id);
+                            WriteToFile(cust.id + ": " + cust.Customer_Login.username + " deleted Sucesfully");
+                            updateDeletedCustomersTable(cust, "Active But Not Operating since " + ((lastlogin != null) ? lastlogin.timein.ToShortDateString() : cust.dateRegistered.ToShortDateString()));
+                        }
+                        else
+                        {
+                            WriteToFile(cust.id + ": " + cust.Customer_Login.username + " lastlogin = " + lastlogin.timein);
+                        }
+                    }
+                    else
+                    {
+                        WriteToFile(cust.id + ": " + cust.Customer_Login.username + "  has transactions or deposit or tickets");
+                    }
+
+
+
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    WriteToFile("Error :" + ex.Message + " on internal");
+                }
+
+
+
+
+
+
+
+            }
+
+
+        }
+
+
+
+
+        public static void DeleteCustomerCascade()
+        {
+            GCL_LOTTODBCONTEXTDataContext db = new GCL_LOTTODBCONTEXTDataContext();
+            //List<Daily_Game> DailyGAmes = new List<Daily_Game>();
+            string daysstring = System.Configuration.ConfigurationManager.AppSettings["InactivityDays"].ToString();
+
+            int days = int.Parse(daysstring);
+
+            DateTime minussevendays = DateTime.Now.AddDays(-days);
+            List<Customer> customers = db.Customers.Where(a => a.Customer_Category1.code == "OC" && a.Customer_Wallet.wallet_balance <= 200 && a.Customer_Login.Customer_Login_Logs.Count < 3 && a.Customer_Login.is_active == false && a.Customer_Login.is_verified == false && a.dateRegistered < minussevendays).ToList<Customer>();
+            WriteToFile("Customer Count within "+ days +" days => "+ customers.Count());
+            for (int i = 0; i<  customers.Count ;  i++)
+            {
+                Customer cust = customers[i];
+                try
+                {
+                    WriteToFile(cust.id + ": " + cust.Customer_Login.username + " begin delete");
+                    if (cust.Customer_Transactions.Count() < 10 && cust.Tickets.Count() == 0 && cust.Customer_Deposits.Count() < 5)
+                    {
+                        Customer_Login_Log lastlogin = cust.Customer_Login.Customer_Login_Logs.OrderByDescending(a => a.id).FirstOrDefault();
+                        if (lastlogin == null || (lastlogin.status == true && lastlogin.timein <= minussevendays))
+                        {
+                            db.deleteCustomer(cust.id);
+                            WriteToFile(cust.id + ": " + cust.Customer_Login.username + " deleted Sucesfully");
+                            updateDeletedCustomersTable(cust, "Inactive and not validated since " + ((lastlogin != null)? lastlogin.timein.ToShortDateString() : cust.dateRegistered.ToShortDateString()) );
+                        }
+                        else
+                        {
+                            WriteToFile(cust.id + ": " + cust.Customer_Login.username + " lastlogin = " + lastlogin.timein);
+                        }
+                    }
+                    else
+                    {
+                        WriteToFile(cust.id + ": " + cust.Customer_Login.username + "  has transactions or deposit or tickets");
+                    }
+
+               
+
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    WriteToFile("Error :" + ex.Message + " on internal");
+                }
+
+                
+
+
+
+                        
+
+            }
+
+
+        }
+
+        private static void updateDeletedCustomersTable(Customer cust, string detail)
+        {
+            Deleted_Customer customers = new Deleted_Customer
+            {
+                email = cust.Customer_Login.email,
+                fullname = cust.lastname + " " + cust.firstname + " " + cust.middlename,
+                username = cust.Customer_Login.email,
+                reasonfordeletion = detail
+            };
+
+            GCL_LOTTODBCONTEXTDataContext db = new GCL_LOTTODBCONTEXTDataContext();
+            db.Deleted_Customers.InsertOnSubmit(customers);
+            db.SubmitChanges();
+        }
+
+        private static void WriteToFile(string text)
+        {
+
+            try
+            {
+                string path = System.Configuration.ConfigurationManager.AppSettings["LogFile"].ToString();
+                //Directory.GetDirectories(path);
+                FileInfo file = new FileInfo(path);
+                file.Directory.Create();
+                if (file.Exists && file.Length > 40000)
+                    file.Delete();
+
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                    writer.Close();
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
